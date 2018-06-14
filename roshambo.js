bd8fdbfbb0c2a61acc81a4ba147ce896b93469ef
Danielle Gamble <daniellehgamble@gmail.com>
2018-06-14 00:19:59 -0700

    objectififed and has matched pairs- doesnt return anything

--- roshambo.js
+++ roshambo.js
@@ -1,35 +1,104 @@
+var Game = {
+    rockButton: document.getElementById("rock"),
+    paperButton: document.getElementById("paper"),
+    scissorsButton: document.getElementById("scissors"),
+    lizardButton: document.getElementById("lizard"),
+    spockButton: document.getElementById("spock"),
+   
 
-var roshambo {
+    score: {
+        wins: 0,
+        losses: 0,
+        ties: 0,
+    }
+    choices: {
+        Rock: 0,
+        Paper: 1,
+        Scissors: 2,
+        Spock: 3,
+        Lizard: 4,
+    }
+    results: {
+        wins: 1,
+        ties: 0,
+        losses: -1,
+    }
+    player: new Player();
+    computer: new Player();
 
+    storePlayerChoice: function (choice) {
+        Game.player.choices = choices;
+        console.log("My choice is " + Game.player.choices);
+        Game.storeComputerChoice();
+    },
+    storeComputerChoice: function () {
+        Game.computer.choice = Math.floor(Math.random() * 3);
+    },
+    playTheGame: function playGame() {
+    if (Game.player.choices != null) {
+        if (Game.player.choices == Game.computer.choices) {
+            ++Game.score.ties;
+            Game.displayGameResult("tie");
+            Game.updateMatches();
+        } else if ( Game.player.choices == Game.player.choices.Rock && (Game.computer.choices == Game.computer.choices.Scissors || Game.computer.choices == Game.computer.choices.Lizard)) {
+            ++Game.score.wins;
+            Game.displayGameResult("win");
+            Game.updateMatches();
+        } else if (Game.player.choices == Game.player.choices.Paper && (Game.computer.choices == Game.computer.choices.Rock || Game.computer.choices == Game.computer.choices.Spock)) {
+            ++Game.score.wins;
+            Game.displayGameResult("win");
+            Game.updateMatches();
+        } else if (Game.player.choices == Game.player.choices.Scissors && (Game.computer.choices == Game.computer.choices.Paper || Game.computer.choices == Game.computer.choices.Lizard)) {
+            ++Game.score.wins;
+            Game.displayGameResult("win");
+            Game.updateMatches();
+        } else if (Game.player.choices == Game.player.choices.Spock && (Game.computer.choices == Game.computer.choices.Scissors || Game.computer.choices == Game.computer.choices.Rock)) {
+            ++Game.score.wins;
+            Game.displayGameResult("win");
+            Game.updateMatches();
+        } else if (Game.player.choices == Game.player.choices.Lizard && (computerChoice == 3 || computerChoice == 1)) {
+            updateScore(0);
+            displayGameResult("win");
+            updateMatches();
+        } else {
+            updateScore(2);
+            displayGameResult("lose");
+            updateMatches();
+        }
+    }
+    playerChoice = null;
+    }
+
+}
+function displayGameResult(result) {
+    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
+    if (result === "win") {
+        document.getElementById("result").textContent = message + " YOU WIN!";
+        document.getElementById("result").className = "alert alert-success";
+    } else if (result === "lose") {
+        document.getElementById("result").textContent = message + " YOU LOSE!";
+        document.getElementById("result").className = "alert alert-danger";
+    } else {
+        document.getElementById("result").textContent = message + " A tie.";
+        document.getElementById("result").className = "alert alert-info";
+    }
+    updateScoreBoard();
+}
+
+function updateScore(val) {
+    ++score[val];
+    console.log("The score is now " + score);
+}
+
+function updateScoreBoard() {
+    document.getElementById("wins").textContent = score[0];
+    document.getElementById("losses").textContent = score[2];
+    document.getElementById("ties").textContent = score[1];
 }
 
 
 
 
-
-// Stores the lables for the choices
-var choices = ["Rock", "Paper", "Scissors"];
-
-// Stores the current player's and computer's choices
-// 0 = Rock, 1 = Paper, 2 = Scissors, 3 = spock, 4 = lizard
-var playerChoice;
-var computerChoice;
-
-// Stores the player's choice
-function storePlayerChoice(choice) {
-    playerChoice = choice;
-    console.log("My choice = " + playerChoice);
-}
-
-
-// The button elements
-var rockButton = document.getElementById("rock");
-var paperButton = document.getElementById("paper");
-var scissorsButton = document.getElementById("scissors");
-var spockButton = document.getElementById("spock");
-var lizardButton = document.getElementById("lizard");
-
-// Add the event handlers
 rockButton.addEventListener('click', () => {
     storePlayerChoice(0)
 });
@@ -46,96 +115,29 @@ lizardButton.addEventListener('click', () => {
     storePlayerChoice(4)
 });
 
-// Generate computer's random choice
-function storeComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3);
-    console.log("Computer choice = " + computerChoice);
-}
-
-// Stores the player's choice, then call's the function for storing the computer's choice
-function storePlayerChoice(choice) {
-    playerChoice = choice;
-    console.log("My choice = " + choice);
-    storeComputerChoice();
-}
-// This is the function for playing the game
-function playGame() {
-    // Here is the game ruleset algorithm
-    if (playerChoice == computerChoice) {
-        // We have a tie!
-        displayGameResult("tie")
-    } else if (playerChoice == 0 && computerChoice == 2) {
-        // Rock beats scissors - a win!
-        displayGameResult("win")
-    } else if (playerChoice == 1 && computerChoice == 0) {
-        // Paper beats scissors - a win!
-        displayGameResult("win")
-    } else if (playerChoice == 2 && computerChoice == 1) {
-        // Scissors beats paper - a win!
-        displayGameResult("win")
-    } else {
-        // All other combinations are losses
-        displayGameResult("lose")
-    }
-}
-// Here is the game ruleset algorithm
-if (playerChoice == computerChoice) {
-    // We have a tie!
-    console.log("tie");
-} else if (playerChoice == 0 && computerChoice == 2) {
-    // Rock beats scissors - a win!
-    console.log("win");
-} else if (playerChoice == 1 && computerChoice == 0) {
-    // Paper beats scissors - a win!
-    console.log("win");
-} else if (playerChoice == 2 && computerChoice == 1) {
-    // Scissors beats paper - a win!
-    console.log("win");
-} else {
-    // All other combinations are losses
-    console.log("lose");
-}
-}
-
-
-
-
-// The button elements
-var rockButton = document.getElementById("rock");
-var paperButton = document.getElementById("paper");
-var scissorsButton = document.getElementById("scissors");
-var playButton = document.getElementById("play");
-
-// Add the event handlers
-rockButton.addEventListener('click', () => {
-    storePlayerChoice(0)
-});
-paperButton.addEventListener('click', () => {
-    storePlayerChoice(1)
-});
-scissorsButton.addEventListener('click', () => {
-    storePlayerChoice(2)
-});
 playButton.addEventListener('click', () => {
     playGame()
 });
 
-//Displays the result of the game
-function displayGameResult(result) {
-    // Create a message for the player
-    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
-    // Add to the base message if it was a win, loss, or tie
-    if (result === "win") {
-        // Display that it was a win
-        document.getElementById("result").textContent = message + " YOU WIN!";
-        document.getElementById("result").className = "alert alert-success";
-    } else if (result === "lose") {
-        // Display that it was a loss
-        document.getElementById("result").textContent = message + " YOU LOSE!";
-        document.getElementById("result").className = "alert alert-danger";
-    } else {
-        // Display that it was a tie
-        document.getElementById("result").textContent = message + " A tie.";
-        document.getElementById("result").className = "alert alert-info";
+var matches = [0, 0];
+
+function updateMatchBoard() {
+    document.getElementById("matchWin").textContent = matches[0];
+    document.getElementById("matchLoss").textContent = matches[1];
+}
+
+function updateMatches() {
+    if (score[0] == 2) {
+        ++matches[0];
+        score[0] = 0;
+        score[1] = 0;
+        score[2] = 0;
+        updateMatchBoard();
+    } else if (score[2] == 2) {
+        ++matches[1];
+        score[0] = 0;
+        score[1] = 0;
+        score[2] = 0;
+        updateMatchBoard();
     }
 }
